38ec9241b0f79f6a2e80e527934ebd654fe24952
Victor Elizalde <vic-bull@hotmail.com>
2021-04-19 15:26:34 -0500

    complete all CRUDs

--- app/controllers/vacancies_controller.rb
+++ app/controllers/vacancies_controller.rb
@@ -1,22 +1,34 @@
 class VacanciesController < ApplicationController
   def index
-    @vacancies = Vacancy.all
+    @vacancies = Vacancy.all.includes(:company)
+  end
+
+  def list_vacancies
+    @vacancies = Vacancy.all.includes(:company)
   end
 
   def show
     @vacancy = Vacancy.find(params[:id])
   end
 
+  def see_vacancy
+    @vacancy = Vacancy.find(params[:id])
+  end
+
   def new
     @vacancy = Vacancy.new
+    @companies = Company.all
   end
 
   def edit
     @vacancy = Vacancy.find(params[:id])
+    @companies = Company.all
   end
 
   def create
     @vacancy = Vacancy.new(vacancy_params)
+    @companies = Company.all
+
     if @vacancy.save
       redirect_to vacancies_path, notice: 'Vacancy created successfully.'
     else
@@ -27,6 +39,7 @@ class VacanciesController < ApplicationController
 
   def update
     @vacancy = Vacancy.find(params[:id])
+    @companies = Company.all
 
     if @vacancy.update(vacancy_params)
       redirect_to vacancies_path, notice: 'Vacancy updated successfully.'
@@ -44,6 +57,6 @@ class VacanciesController < ApplicationController
 
   private
     def vacancy_params
-      params.require(:vacancy).permit(:title, :description, :min_salary, :max_salary, :published_at)
+      params.require(:vacancy).permit(:title, :description, :min_salary, :max_salary, :published_at, :company_id)
     end
 end
