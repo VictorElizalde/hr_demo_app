742b90a3f8295674e2c03e093b5cd4d247883d2a
Victor Elizalde <vic-bull@hotmail.com>
2021-04-30 17:11:00 -0500

    fix

---
+++ app/controllers/admin/applications_controller.rb
@@ -0,0 +1,25 @@
+class Admin::ApplicationsController < ApplicationController
+  include CheckAdminConcern
+  before_action :redirect_unless_admin, only: [:update, :index]
+
+  def index
+    @applications = Application.all.includes(:candidate, :vacancy)
+  end
+
+  def update
+    @application = Application.find(params[:id])
+
+    if @application.update(application_update_params)
+      redirect_to admin_applications_path, notice: 'Application updated successfully.'
+    else
+      flash[:alert] = 'Application not updated'
+      render 'index'
+    end
+  end
+
+  private
+
+  def application_update_params
+    params.require(:application).permit(:status)
+  end
+end
--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationsController < ApplicationController
-  include CheckAdminConcern
-  before_action :redirect_unless_admin, only: [:update, :index]
-
-  def index
-    @applications = Application.all.includes(:candidate, :vacancy)
-  end
-
   def new
     @vacancy = Vacancy.find(params[:vacancy_id])
     @application = Application.new
@@ -26,26 +19,11 @@ class ApplicationsController < ApplicationController
     end
   end
 
-  def update
-    @application = Application.find(params[:id])
-
-    if @application.update(application_update_params)
-      redirect_to applications_path, notice: 'Application updated successfully.'
-    else
-      flash[:alert] = 'Application not updated'
-      render 'index'
-    end
-  end
-
   private
 
   def application_params
     params.require(:application).permit(:personal_site, :cv, :bio, :candidate_id, :vacancy_id, :status,
       candidate_attributes: [:id, :name, :last_name, :address, :_destroy]
     )
-  end
-
-  def application_update_params
-    params.require(:application).permit(:status)
   end
 end
